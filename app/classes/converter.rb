38eff70967e2d544df72a73b98801604ff64524d
Adam <ar@getautomata.com>
2017-06-07 16:00:40 +0100

    Add basic converter class function

--- app/classes/converter.rb
+++ app/classes/converter.rb
@@ -1,4 +1,17 @@
 class Converter
 
-  
+  def initialize(folder, destination_folder)
+    @dir = folder
+    @new_dir = destination_folder
+  end
+
+  def convert!
+    Dir.foreach(dir) do |item|
+      next if item == '.' or item == '..'
+      unless item.include?(".DS_Store")
+        Libreconv.convert("#{dir}/#{item}", new_dir, '/Applications/LibreOffice.app/Contents/MacOS/soffice')
+      end
+    end
+  end
+
 end
